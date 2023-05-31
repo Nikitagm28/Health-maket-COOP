8ccdf6e7cfc406ddd655e7dedf4e52b7332a8ce7
LebArt2321 <fifa.artem.lebedev@mail.ru>
2023-05-31 13:08:27 +0300

    все еще страничка логина

--- login.py
+++ login.py
@@ -3,14 +3,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-
-
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
+
 
 class LoginScreen(BoxLayout):
     def __init__(self, **kwargs):
@@ -36,12 +31,15 @@ class LoginScreen(BoxLayout):
 
         # добавляем кнопку
         button_layout = BoxLayout(size_hint=(0.5, None), height=50, pos_hint={"center_x": 0.5})
-        button_layout.add_widget(Button(text="Войти", background_color = (0, 1, 0, 0.75)))
+        button_layout.add_widget(Button(text="Войти", background_color = (0, 1, 0, 1)))
         self.add_widget(button_layout)
 
         button_layout = BoxLayout(size_hint=(0.5, None), height=50, pos_hint={"center_x": 0.5})
-        button_layout.add_widget(Button(text="Регистрация", background_color = (0, 1, 0, 0.9)))
+        button_layout.add_widget(Button(text="Регистрация", background_color = (0, 0.5, 0, 1)))
         self.add_widget(button_layout)
+
+        spacer = Widget(size_hint=(1, None), height=50)
+        self.add_widget(spacer)
 
 class LoginApp(App):
     def build(self):
